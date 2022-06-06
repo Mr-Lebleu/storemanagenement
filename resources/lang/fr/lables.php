413296a79faa050b558c71d86601b12874334ac8
Orleien <orelien.soany@gmail.com>
2022-06-06 20:12:32 +0300

    first commit

---
+++ resources/lang/fr/lables.php
@@ -0,0 +1,319 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    'general-backtotop' => 'Back To Top',
+
+    
+    
+    'header-top-offer' => 'Obtenez <strong> JUSQU\'À 10 % DE RÉDUCTION </strong> sur votre 1ère commande
+    <div class="pro-link-dropdown js-toppanel-link-dropdown">
+      <a href="/shop" class="pro-dropdown-toggle" style="cursor:pointer">
+        Details
+      </a>
+    </div>',
+    'header-welcome-text' => 'Bienvenue',
+    'header-welcome-user' => 'Bienvenue utilisateur !',
+    'header-login-register' => 'Connexion/S\'inscrire',
+    'header-login' => 'Connexion',
+    'header-register' => 'S\'inscrire',
+    'header-my-account' => 'Mon compte',
+    'header-profile' => 'Profil',
+    'header-wishlist' => 'Liste de souhaits',
+    'header-compare' => 'Comparer',
+    'header-order' => 'Commandes',
+    'header-shipping-address' => 'Adresse de livraison',
+    'header-change-password' => 'Modifier le mot de passe',
+    'header-logout' => 'Déconnexion',
+    'header-all-categories' => 'TOUTES LES CATÉGORIES',
+    'header-cart' => 'Panier',
+    'header-emptycart' => 'Vous n\'avez aucun article dans votre panier.',
+    'header-item' => 'Articles',
+    'header-total' => 'Total',
+    'header-checkout' => 'Commander',
+    'header-view-cart' => 'Voir le panier',
+    'header-search-products' => 'Rechercher des produits',
+    'header-call-us-now' => 'Appelez-nous maintenant',
+    'header-free-shipping' => 'Livraison gratuite',
+    'header-over-order' => 'Sur commande supérieure à 99$',
+    'header-header2-phone' => 'Ligne d\'assistance',
+
+    'home-slider-button-title'=>"Acheter maintenant",
+
+    'home-product-categories-title'=>"CATÉGORIES DE PRODUITS",
+    'home-product-categories-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis felis tempus feugiat maximus.",
+    'home-banner-title'=>"SNEAKER",
+    'home-banner-sub-title'=>"Chaussures de couple",
+    'home-button-text'=>"Afficher toute la gamme",
+
+    'home-new-arrival-title'=>"NOUVEL ARRIVAGE",
+    'home-new-arrival-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis felis tempus feugiat maximus.",
+
+    'home-news-title'=>"ACTUALITÉS ET ÉVÉNEMENTS",
+
+
+    'home-news-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi venenatis felis tempus feugiat maximus.",
+
+    'home-tab-title'=>"BIENVENUE DANS LA BOUTIQUE",
+    'home-tab-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi venenatis felis tempus feugiat maximus.",
+    'home-tab-topsales'=>"Meilleures ventes",
+    'home-tab-special'=>"Spécial",
+    'home-tab-most-liked' => 'Le plus aimé',
+    
+    'home-tab-top-title'=>"MEILLEURES VENTES DE LA SEMAINE",
+    'home-tab-top-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi venenatis felis tempus feugiat maximus.",
+
+    'home-services-free-shipping'=>"Livraison gratuite",
+    'home-services-on-ordr'=>"Sur commande supérieure à 99 $",
+    'home-services-money-return'=>"Retour d'argent",
+    'home-services-return-days'=>"Remboursement sous 30 jours",
+    'home-services-support'=>"Assistance 24/7",
+    'home-services-hotline'=>"Ligne d'assistance",
+    'home-services-safe-payment'=>"Paiement sécurisé",
+    'home-services-protect-payment'=>"Protéger le paiement en ligne",
+
+
+
+    'home-weekly-sale-heading'=>"MEILLEURE VENTE DE LA SEMAINE",
+    'home-weekly-sale-description'=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis felis tempus feugiat maximus.",
+
+
+
+    'footer-quick-links'=>'Liens rapides',
+    'footer-home'=>'Accueil',
+    'footer-shop'=>'Magasin',
+    'footer-orders'=>'Commandes',
+    'footer-shopping-cart'=>'Panier',
+    'footer-wishlist'=>'Liste de souhaits',
+    'footer-personnalisation' => 'Personnalisation',
+    'footer-instagram-feed' => 'Flux Instagram',
+    'footer-company' => 'Site Web 2021, Tous droits réservés.',
+    'footer-privacy' => 'Confidentialité',
+    'footer-term' => 'Conditions',
+    'footer-about-store' => 'À propos du magasin',
+    'footer-our-services' => 'Nos services',
+    'footer-informations' => 'Informations',
+    'footer-newsletter' => 'Newsletter',
+    'footer-follow-us' => 'Suivez-nous',
+    'footer-download-our-app' => 'TÉLÉCHARGER NOTRE APPLICATION',
+    'footer-category' => 'catégories',
+    'footer-payments'=>'Nous utilisons des paiements sécurisés',
+    'footer-contact-us' => 'Contactez-nous',
+    'footer-latest-subscribe'=>'ABONNEZ-VOUS POUR LES DERNIÈRES MISES À JOUR',
+    'footer-subscribe' => 'S`\'abonner',
+    'footer-safe-payment' => 'Nous utilisons des paiements sécurisés',
+    'footer-facebook' => 'Facebook',
+    'footer-twitter' => 'Twitter',
+    'footer-google' => 'Google',
+    'footer-linkedin' => 'LinkedIn',
+    'footer-instagram' => 'Instagram',
+
+
+    
+
+
+    
+
+
+
+
+    'shop-shop'=>'Boutique',
+    'shop-display' => 'Afficher',
+    'shop-category' => 'Catégorie',
+    'shop-price' =>'Prix',
+    'shop-sort-by' => 'Trier par',
+    'shop-brands' => "Marque",
+    'shop-apply'=>'Appliquer',
+    'shop-reset'=>'reset',
+
+    'cart-page-shopping-cart'=>'Panier',
+    'cart-page-apply'=>'Appliquer',
+    'cart-page-continue-shopping'=>'CONTINUER VOS ACHATS',
+    'cart-page-update-cart'=>'METTRE À JOUR LE PANIER',
+    'cart-page-proceed-to-checkout'=>'PASSER A LA CAISSE',
+    'cart-page-order-summary'=>'Récapitulatif de la commande',
+    'cart-page-subtotal'=>'Sous-total',
+    'cart-page-discount'=>'Remise(Coupon)',
+    'cart-page-total'=>'Total',
+    'product-detail-product'=>"Produit",
+    'product-detail-related-product-title'=>'Produits associés',
+    'product-detail-related-product-description'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Morbi venenatis felis tempus feugiat maximus.',
+
+
+
+
+    'checkout-checkout'=>'CHECKOUT',
+    'checkout-shipping-address'=>'Adresse de livraison',
+    'checkout-billing-address'=>'Adresse de facturation',
+    'checkout-shipping-method'=>'Méthodes d`\'expédition',
+    'checkout-order-detail'=>'Détails de la commande',
+
+
+    'checkout-first-name'=>'Prénom',
+    'checkout-last-name'=>'Nom de famille',
+    'checkout-address'=>'Adresse',
+    'checkout-country-name'=>'Nom du pays',
+    'checkout-state-name'=>'Nom de l`\'état',
+    'checkout-city-name'=>'Nom de la ville',
+    'checkout-postal-code'=>'Code postal',
+    'checkout-phone'=>'Téléphone',
+
+
+    'checkout-billing-first-name'=>'Prénom',
+    'checkout-billing-last-name'=>'Nom de famille',
+    'checkout-billing-country-name'=>'Nom du pays',
+    'checkout-billing-state-name'=>'Nom de l\'État',
+    'checkout-billing-city-name'=>'Nom de la ville',
+    'checkout-billing-postal-code'=>'Code postal',
+    'checkout-billing-phone'=>'Téléphone',
+    'shipping-address-latlong'=>'Lat/Lng',
+    'checkout-continue'=>'Continuer',
+    'checkout-back'=>'Retour',
+
+
+
+    'checkout-order-notes-title'=>'Notes et résumé de la commande',
+    'checkout-order-notes-description'=>'Veuillez écrire des notes de votre
+    ordre',
+    'checkout-payment-method-title'=>'Modes de paiement',
+    'checkout-payment-method-description'=>'Méthodes d`\'expédition',
+
+    'checkout-order-summary'=>'Récapitulatif de la commande',
+    'checkout-subtotal'=>'Sous-total',
+    'checkout-discount'=>'Remise(Coupon)',
+    'checkout-total'=>'Total',
+    'checkout-shipping'=>'Livraison',
+    'checkout-tax'=>"Taxe",
+    'checkout-same-ship-bill-address-text'=>'Même adresse de livraison et de facturation.',
+    'checkout-location' => 'Emplacement',
+    'checkout-location-placeholder'=>'Veuillez saisir votre emplacement ou cliquer ici pour ouvrir la carte',
+
+
+    'login-sign-up' => 'S`\'inscrire',
+    'login-login' => 'Connexion',
+    'login-email' =>'Adresse e-mail',
+    'login-password' => 'Mot de passe',
+    'login-new-customer' => 'Nouveau client',
+    'login-first-name' => 'Prénom',
+    'login-last-name' => 'Nom de famille',
+    'login-confirm-password' => 'Confirmer le mot de passe',
+    'login-create-account' => 'Créer un compte',
+    'login-access-account' => 'Accéder à votre compte via vos réseaux sociaux',
+    'login-forget-password' =>'Mot de passe oublié',
+    'profile-side-menue-profile' =>'Profil',
+    'profile-side-menue-wishlist' =>'Liste de souhaits',
+    'profile-side-menue-compare' =>'Comparer',
+    'profile-side-menue-orders' =>'Commandes',
+    'profile-side-menue-shipping-address' =>'Adresse de livraison',
+    'profile-side-menue-change-password' =>'Modifier le mot de passe',
+    'profile-side-menue-logout' =>'Déconnexion',
+    
+
+    'profile-email'=> "E-mail",
+    'profile-my-account'=> "Mon compte",
+    'profile-prsonal-info'=> "Informations personnelles",
+    'profile-first-name' => "Prénom",
+    'profile-last-name' => 'Nom de famille',
+    'profile-gender' => 'Sexe',
+    'profile-dob' => 'date de naissance',
+    'profile-phone' => 'Téléphone',
+    'profile-update' => 'Mettre à jour',
+    'profile-date-check' => 'Désolé ! Les mineurs ont besoin de conseils parentaux pour utiliser ce site Web',
+
+
+    'orders-my-account' => 'Mon compte',
+    'orders-my-order'=>'Ma commande',
+    'orders-date' => 'Date de commande',
+    'orders-description' => 'Description',
+    'orders-order-id'=>'Numéro de commande',
+    'orders-status'=>'Statut',
+    'orders-detail'=>'Détail',
+
+
+    'order-detail' =>"Détail de la commande",
+    'order-detail-heading'=>"Informations sur la commande",
+    'order-detail-my-account' => 'Mon compte',
+    'order-detail-order-id' => 'Numéro de commande',
+    'order-detail-order-status' => 'Statut de la commande',
+    'order-detail-order-date' => 'Date de commande',
+    'order-detail-shipping-detail' => 'Détails de livraison',
+    'order-detail-billing-detail' => 'Détails de facturation',
+    'order-detail-payment-shipping-method' => 'Paiement',
+    'order-detail-item' => 'ITEM"S',
+    'order-detail-price' => 'PRIX',
+    'order-detail-qty' => "QTÉ",
+    'order-detail-subtotal' => 'SOUS-TOTAL',
+    'order-detail-comments' => 'Commentaires',
+
+
+    'thank-you-title'=>'Merci',
+    'thank-you-message' => 'Vous avez passé votre commande avec succès.
+    Allez à la',
+    'thank-you-order-page' => 'Page de commande',
+
+
+    'shipping-address'=>'Adresse de livraison',
+    'shipping-address-my-account' =>'Mon compte',
+    'shipping-address-default' =>'DEFAUT',
+    'shipping-address-first-name' =>'Prénom',
+    'shipping-address-last-name' =>'Nom de famille',
+    'shipping-address-country-state-city' =>'Pays État Ville',
+    'shipping-address-action' =>'ACTION',
+    'shipping-address-add' =>'Gérer l`\'adresse de livraison',
+    'shipping-address-stret-address' =>'Adresse municipale',
+    'shipping-address-country' =>'Sélectionner un pays',
+    'shipping-address-state' =>'Sélectionner l\'état',
+    'shipping-address-city' =>'Ville',
+    'shipping-address-postal-code' =>'Code postal',
+    'shipping-address-add-address' =>'Soumettre',
+
+
+
+
+    'change-password'=>'Modifier le mot de passe',
+    'change-password-current-password'=>'Mot de passe actuel',
+    'change-password-new-password'=>'Nouveau mot de passe',
+    'change-password-confirm-password'=>'Confirmer le mot de passe',
+    'change-password-access-text'=>'Accéder à votre compte via vos réseaux sociaux',
+
+
+    'forget-password'=>'Oublier le mot de passe',
+    'forget-password-email'=>'E-mail',
+    'forget-password-verify'=>"varier l'adresse e-mail",
+
+  
+    'bread-crumb-home'=>"Accueil",
+    'bread-crumb-shop'=>'Magasin',
+    'pain-panier'=>'PANIER',
+    'bread-product-page'=>'Page produit',
+    'bread-blog'=>'Blog',
+    'bread-checkout'=>'Commander',
+    'bread-login'=>'login',
+    'bread-compare'=>'Comparer',
+    'bread-profile'=>'Profil',
+    'pain-commande'=>'Commande',
+    'wishlist-add-success' => 'Produit ajouté à la liste de souhaits !',
+    'wishlist-remove' => 'Produit supprimé de la liste de souhaits !',
+
+
+
+
+
+
+];
